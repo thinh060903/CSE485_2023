470ca81dfcef531a03b8016ea29dcfeda9a7238b
Cù Thịnh <152584089+thinh060903@users.noreply.github.com>
2023-12-16 14:14:10 +0700

    Delete TH3 directory

--- TH3/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Database configuration
-define('DB_HOST', 'your_database_host');
-define('DB_NAME', 'your_database_name');
-define('DB_USER', 'your_database_user');
-define('DB_PASS', 'your_database_password');
-?>
